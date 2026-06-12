1529bada32d7ba3a6061396f19f276d9fc83d66b
Midnight-Poet <kaydeymyrtle@gmail.com>
2026-06-12 07:34:02 +0100

    Delete splendid-puff directory

--- splendid-puff/src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { Toast } from './Toast';
-export { OrderForm } from './OrderForm';
-export { TrackOrder } from './TrackOrder';
-export { CustomerApp } from './CustomerApp';
-export { AdminApp } from './AdminApp';
-export { AdminLock } from './AdminLock';
-export { AdminPanel } from './AdminPanel';
-export { OrderCard } from './OrderCard';
