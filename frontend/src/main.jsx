5ec307e35c90e4590f5445c4f8e5337b3fa9bfbe
= <kaydeymyrtle@gmail.com>
2026-06-21 18:05:32 +0100

    Minor changes: File ext added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import { ToastProvider } from './components/Toast';
-import OrderPage from './pages/OrderPage';
-import AdminLock from './pages/admin/AdminLock';
-import AdminShell from './pages/admin/AdminShell';
-import AdminOrders from './pages/admin/AdminOrders';
-import AdminOrderDetail from './pages/admin/AdminOrderDetail';
-import AdminProducts from './pages/admin/AdminProducts';
+import OrderPage from './pages/OrderPage.jsx';
+import AdminLock from './pages/admin/AdminLock.jsx';
+import AdminShell from './pages/admin/AdminShell.jsx';
+import AdminOrders from './pages/admin/AdminOrders.jsx';
+import AdminOrderDetail from './pages/admin/AdminOrderDetail.jsx';
+import AdminProducts from './pages/admin/AdminProducts.jsx';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
@@ -19,8 +19,10 @@ createRoot(document.getElementById('root')).render(
 				<BrowserRouter>
 					<Routes>
 						<Route path='/' element={<OrderPage />} />
+						{/* <Route path='/admin' element={<AdminLock />} /> */}
 						<Route path='/admin' element={<AdminLock />} />
 						<Route path='/admin/*' element={<AdminShell />}>
+
 							<Route element={<AdminOrders />} />
 							<Route path='orders' element={<AdminOrders />} />
 							<Route
